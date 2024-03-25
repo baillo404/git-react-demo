8f6377f1e15a3502d56371eb6a9714706fdc8c00
Alpho Bakos <alphooumardiallo765@gmail.com>
2024-03-25 10:49:36 +0000

    first modify

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h1>Mon projet git</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio maiores quibusdam dolor quis sed, deserunt culpa laudantium libero, porro ad aut. Non laborum repellendus sunt vero cumque quibusdam asperiores!</p>
+      <button>Click me</button>
     </>
   )
 }
