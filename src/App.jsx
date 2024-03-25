b6609a2fc1bd033470f760aa76271160b6a2b54d
Alpho Bakos <alphooumardiallo765@gmail.com>
2024-03-25 10:29:24 +0000

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Mon projet git</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio maiores quibusdam dolor quis sed, deserunt culpa laudantium libero, porro ad aut. Non laborum repellendus sunt vero cumque quibusdam asperiores!</p>
     </>
   )
 }
